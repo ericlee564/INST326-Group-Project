0bcc4a54534b402ae8653ae9f54c0f9eca9afc74
David McCoy <davidmccoy@davids-mbp-3.lan>
2020-11-10 18:42:10 -0500

    Added two functions

--- finalproject.py
+++ finalproject.py
@@ -2,15 +2,33 @@
 
 class store():
     
-    def __init__(self):
+    def __init__(self, products):
 
 
-    def inventory():
+    def inventory(item, price, amount):
         """ Creates a dictionary of all products within the store and its prices
+        
+        Args:
+            item(str): string of item name
+            price(int): price of item
+            amount(int): number of items in inventory
         """
     
-    def addinventory():
+    def addinventory(item, price, amount):
         """ Allows employees to add products to current inventory list
+        
+        Args:
+            item(str): string of new item name
+            price(int): price of new item
+            amount(int): number of new items in inventory
         """
 
+    def order(limit=10):
+    '''Creates an inventory of items that are running low
+    
+    Args:
+        limit(int): amount of inventory that the user needs to order more at
+    '''
+    
+    
     
