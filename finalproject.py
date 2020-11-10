e98e159d088d7df930e1691fc539f66ec09c9aef
Eric Lee <ericlee@erics-mbp.myfiosgateway.com>
2020-11-10 18:22:20 -0500

    added finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -1,1 +1,16 @@
 #Hyungik Lee, David McCoy, Noah Berman, Kiranpreet Kaur
+
+class store():
+    
+    def __init__(self):
+
+
+    def inventory():
+        """ Creates a dictionary of all products within the store and its prices
+        """
+    
+    def addinventory():
+        """ Allows employees to add products to current inventory list
+        """
+
+    
