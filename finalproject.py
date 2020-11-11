23f4b8ef5854b68085755bbc50135996339736b1
Noah Berman <ngberman@umd.edu>
2020-11-10 19:21:51 -0500

    Added methods for discounting and coupons

--- finalproject.py
+++ finalproject.py
@@ -1,29 +1,31 @@
 #Hyungik Lee, David McCoy, Noah Berman, Kiranpreet Kaur
 
 class store():
-    
+    """Creates a dictionary of inventory from given file
+    Attributes:
+        """
     def __init__(self, products):
 
 
-    def inventory(item, price, amount):
+    def inventory(self, item, price, amount):
         """ Creates a dictionary of all products within the store and its prices
         
         Args:
             item(str): string of item name
             price(int): price of item
             amount(int): number of items in inventory
+        Returns:
+            dictionary of inventory
         """
     
-    def addinventory(item, price, amount):
+    def addinventory(self, filename):
         """ Allows employees to add products to current inventory list
         
         Args:
-            item(str): string of new item name
-            price(int): price of new item
-            amount(int): number of new items in inventory
+            filename (str): name of file
         """
 
-    def order(limit=10):
+    def order(self, limit=10):
     '''Creates an inventory of items that are running low
     
     Args:
@@ -31,22 +33,37 @@ class store():
     
     '''
     
-    def stocked(limit,categories):
-        """
-        This function keeps tracks of how many item we have in our current stock 
-        by different categories
-        Agrs:
-            limit(int): Number of items store have in the stock
-            categories(list): different categories in the stock
+def stocked(limit,categories):
+    """
+    This function keeps tracks of how many item we have in our current stock 
+    by different categories
+    Args:
+        limit(int): Number of items store have in the stock
+        categories(list): different categories in the stock
+    
+    """
+    
+def num_item_sold(item, amountsold):
+    """This function keeps tracks of number of all items sold 
+    Args:
+        item(int): different item types in the stock
+        amountsold(int): number of items sold 
+    """    
         
-        """
-    def num_item_sold(item, amountsold):
-        """This function keeps tracks of number of all items sold 
-            item(int): different item types in the stock
-            amountsold(int): number of items sold 
-           
-        """    
+def item_discount(total_cost):
+    """Generates a discount on the items ordered
+    Args:
+        total_cost (float): total cost of all items ordered
+    Returns: 
+        String with the given discount for specific product"""
         
+def coupon_generator(item, category):
+    """Creates coupon for specific category of food
+    Args:
+        item (str): name of item for coupon
+        category (str): type of category of food within grocery store
+    Returns:
+        String of the item discounted"""
     
     
     
