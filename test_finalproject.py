257c625dd4755f27b8af54c8b64f9218d1e16044
David McCoy <davidmccoy444@gmail.com>
2020-12-22 11:24:10 -0500

    added low inventory test

--- test_finalproject.py
+++ test_finalproject.py
@@ -10,3 +10,24 @@ def test_update_stock():
     assert i.loc[3]["Units Sold"] == 20
     assert i.loc[4]["Units Sold"] == 7
     
+def test_low_inventory():
+    x = StoreInventory('inventory.csv', 'item_sold.csv')
+    lowest_number = x.low_inventory(10)
+    assert lowest_number.iloc[0]['Item Name'] == 'Watermelon'
+    middle_number = x.low_inventory(50)
+    assert middle_number.iloc[0]['Item Name'] == 'Strawberry'
+    assert middle_number.iloc[1]['Item Name'] == 'Blueberry'
+    assert middle_number.iloc[3]['Item Name'] == 'Tomatoes'
+    high_number = x.low_inventory(100)
+    assert high_number.iloc[0]['Item Name'] == 'Strawberry'
+    assert high_number.iloc[4]['Item Name'] == 'Lettuce'
+    assert high_number.iloc[8]['Item Name'] == 'Chips'
+    assert high_number.iloc[10]['Item Name'] == 'Gummy Worms'
+    highest_number = x.low_inventory(300)
+    assert highest_number.iloc[0]['Item Name'] == 'Strawberry'
+    assert highest_number.iloc[4]['Item Name'] == 'Lettuce'
+    assert highest_number.iloc[8]['Item Name'] == 'Chips'
+    assert highest_number.iloc[10]['Item Name'] == 'Gum'
+    assert highest_number.iloc[11]['Item Name'] == 'Water'
+    assert highest_number.iloc[12]['Item Name'] == 'Cola'
+    assert highest_number.iloc[13]['Item Name'] == 'Gummy Worms'
