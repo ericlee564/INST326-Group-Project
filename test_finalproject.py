f970404eea423a4b282d67da0a2503b12286cf4b
Eric Lee <ericlee564@yahoo.com>
2020-12-22 11:23:00 -0500

    updated test_update_function

--- test_finalproject.py
+++ test_finalproject.py
@@ -5,3 +5,8 @@ def test_update_stock():
     x = StoreInventory("inventory.csv", "Item_sold.csv")
     i = x.update_stocked()
     assert i.iloc[0]["Units Sold"] == 10
+    assert i.loc[1]["Units Sold"] == 7
+    assert i.loc[2]["Units Sold"] == 9
+    assert i.loc[3]["Units Sold"] == 20
+    assert i.loc[4]["Units Sold"] == 7
+    
