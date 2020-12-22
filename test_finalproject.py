7f40b1c939c8bd0bbf6f681e1f6422c2fa89e0f9
Eric Lee <ericlee564@yahoo.com>
2020-12-22 11:19:35 -0500

    Added test_update_stock

--- test_finalproject.py
+++ test_finalproject.py
@@ -1,1 +1,7 @@
+from finalproject import StoreInventory 
+import pandas as pd
 
+def test_update_stock():
+    x = StoreInventory("inventory.csv", "Item_sold.csv")
+    i = x.update_stocked()
+    assert i.iloc[0]["Units Sold"] == 10
